a0b5f8274265a707a6a9c54cc67aa17a07a66cc7
Pedro Mello <pedromellogomes@gmail.com>
2023-01-13 23:56:51 -0300

    1920_build_array_from_permutation in c lang

---
+++ 1920_build_array_from_permutation.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* build_array(int* nums, int numsSize, int* returnSize)
+{
+    int *ans = (int*) malloc(numsSize * sizeof(int));
+    
+    for(int i = 0; i < numsSize; i++)
+    {
+        ans[i] = nums[nums[i]];
+    }
+
+    *returnSize = numsSize;
+    return ans;
+}
